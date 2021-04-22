41b4d22e746455643622ba1ef1bba2aa84e4141b
John Kenedy <john.kenedy0410@gmail.com>
2021-04-21 23:58:51 -0300

    VerticalNavigation Animation

--- app/src/main/java/com/am_developer/ammarket/activities/MainActivity.kt
+++ app/src/main/java/com/am_developer/ammarket/activities/MainActivity.kt
@@ -1,7 +1,10 @@
-package com.am_developer.ammarket
+package com.am_developer.ammarket.activities
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import com.am_developer.ammarket.R
 import com.am_developer.ammarket.databinding.ActivityMainBinding
 import com.am_developer.ammarket.fragment.*
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
@@ -12,6 +15,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var chipNavigationBar: ChipNavigationBar
     private lateinit var verticalChipNavigationBar: ChipNavigationBar
+
+    private var upDownMenu = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +31,13 @@ class MainActivity : AppCompatActivity() {
         chipNavigationBar.setItemSelected(R.id.navigation_home, true)
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment())
             .commit()
+
+        val infinity = AnimationUtils.loadAnimation(this, R.anim.infinity)
+        binding.infinity.animation = infinity
+
+        binding.downUpMenu.setOnClickListener {
+            btnMenuAnimation()
+        }
 
         bottomMenu()
         verticalMenu()
@@ -54,9 +67,10 @@ class MainActivity : AppCompatActivity() {
     private fun verticalMenu() {
 
         verticalChipNavigationBar.setOnItemSelectedListener(object :
-        ChipNavigationBar.OnItemSelectedListener {
+            ChipNavigationBar.OnItemSelectedListener {
             override fun onItemSelected(id: Int) {
                 val fragment = when (id) {
+                    R.id.navigation_search -> SearchFragment()
                     R.id.navigation_cart -> ShoppingFragment()
                     R.id.navigation_coupon -> CouponFragment()
                     R.id.navigation_list -> ShoppingListFragment()
@@ -70,4 +84,23 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+    private fun btnMenuAnimation() {
+
+        if (upDownMenu == 1) {
+
+            val downMenu = AnimationUtils.loadAnimation(this, R.anim.down_menu)
+            binding.verticalNavMain.animation = downMenu
+            binding.downUpMenu.setImageResource(R.drawable.arrow_up)
+            binding.verticalNavMain.visibility = View.VISIBLE
+            upDownMenu = 2
+        } else {
+            val upMenu = AnimationUtils.loadAnimation(this, R.anim.up_menu)
+            binding.verticalNavMain.animation = upMenu
+            binding.downUpMenu.setImageResource(R.drawable.arrow_down)
+            upDownMenu = 1
+
+        }
+    }
+
 }
