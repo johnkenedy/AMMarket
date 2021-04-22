a06e88bc379d0ea3510c88c5af6be72783b396d8
John Kenedy <john.kenedy0410@gmail.com>
2021-04-22 17:14:41 -0300

    SplashActivity.kt

--- app/src/main/java/com/am_developer/ammarket/activities/SplashActivity.kt
+++ app/src/main/java/com/am_developer/ammarket/activities/SplashActivity.kt
@@ -1,12 +1,42 @@
 package com.am_developer.ammarket.activities
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.am_developer.ammarket.R
+import com.am_developer.ammarket.databinding.ActivitySplashBinding
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        val downSplashLogo = AnimationUtils.loadAnimation(this, R.anim.splash_logo)
+        binding.splashLogo.animation = downSplashLogo
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                finish()
+            }, 7800
+        )
     }
 }
