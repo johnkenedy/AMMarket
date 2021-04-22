4d42ae0bfd5b15b4b71acf0f6e3db1d21e2b7a6c
John Kenedy <john.kenedy0410@gmail.com>
2021-04-22 16:14:09 -0300

    Firebase setup

---
+++ app/src/main/java/com/am_developer/ammarket/activities/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.am_developer.ammarket.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.am_developer.ammarket.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
